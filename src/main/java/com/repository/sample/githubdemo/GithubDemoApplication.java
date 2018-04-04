c61e3d828ccd7188bab8fddb05f1c7d344f519fe
chritian.silerio <densilerio15@gmail.com>
2018-04-04 17:21:52 +0800

    Github demo

---
+++ src/main/java/com/repository/sample/githubdemo/GithubDemoApplication.java
@@ -0,0 +1,12 @@
+package com.repository.sample.githubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDemoApplication.class, args);
+	}
+}
