9b3802fc655d3b380821f28cbec057513329a9fd
chritian.silerio <densilerio15@gmail.com>
2018-04-05 13:20:34 +0800

    testing

--- src/main/java/com/repository/sample/githubdemo/GithubDemoApplication.java
+++ src/main/java/com/repository/sample/githubdemo/GithubDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubDemoApplication {
-
+//testing gthub test
 	public static void main(String[] args) {
 		SpringApplication.run(GithubDemoApplication.class, args);
 	}
